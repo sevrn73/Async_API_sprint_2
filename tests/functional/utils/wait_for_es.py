13dcb524953a4b02e6be684d605db0eefe1b02fd
sevrn73 <57312403+sevrn73@users.noreply.github.com>
2022-12-25 02:45:49 +0300

    start

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -1,20 +1,27 @@
 import asyncio
 from elasticsearch import AsyncElasticsearch
 import logging
-from functional.settings import TEST_SETTINGS
-logger = logging.getLogger('tests')
+from settings import TEST_SETTINGS
+
+logger = logging.getLogger("tests")
+
 
 async def wait_for_es():
-    es_client = AsyncElasticsearch(hosts=[TEST_SETTINGS.es_host, ], validate_cert=False, use_ssl=False)
+    es_client = AsyncElasticsearch(
+        hosts=[
+            TEST_SETTINGS.es_host,
+        ],
+        validate_cert=False,
+        use_ssl=False,
+    )
     response = await es_client.ping()
     while not response:
         await asyncio.sleep(2)
-        print('Elastic is unavailable - sleeping')
-        logger.info('Elastic is unavailable - sleeping')
+        print("Elastic is unavailable - sleeping")
+        logger.info("Elastic is unavailable - sleeping")
         response = await es_client.ping()
     await es_client.close()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(wait_for_es())
